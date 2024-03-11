5d85d6f80457c134e050cba0f35c5c1684dd6a24
Ünal Kaan Başak <149206060+kaanbasak@users.noreply.github.com>
2024-03-11 09:26:45 +0300

    Update c_lab4.1.c

--- c_lab4/c_lab4.1.c
+++ c_lab4/c_lab4.1.c
@@ -2,7 +2,14 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+void triangleCheck(float edge1, float edge2, float edge3, float sum);
 
+int main(){
+float edge1, edge2, edge3, sum;
+triangleCheck(edge1,edge2,edge3,sum);
+
+    return 0;
+}
 void triangleCheck(float edge1, float edge2, float edge3, float sum){
 
 srand(time(NULL));
@@ -14,11 +21,4 @@ printf(" edge1: %.f\n edge2: %.f\n edge3: %.f\n",edge1,edge2,edge3);
 sum = fabs(edge1 - edge2) < edge3 && edge3 < (edge1 + edge2) && fabs(edge1 - edge3) < edge2 && edge2 < (edge1 + edge3) && fabs(edge3 - edge2) < edge1 && edge1 < (edge3 + edge2);
 printf("Do these wires form a triangle? %.f ",sum);
 
-}
-
-int main(){
-float edge1, edge2, edge3, sum;
-triangleCheck(edge1,edge2,edge3,sum);
-
-    return 0;
 }
