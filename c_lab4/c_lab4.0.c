5656fe40824e0907933668183d26ab590bad7955
Ünal Kaan Başak <149206060+kaanbasak@users.noreply.github.com>
2024-03-11 09:28:07 +0300

    Update c_lab4.0.c

--- c_lab4/c_lab4.0.c
+++ c_lab4/c_lab4.0.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+void isAlphabet(char ch, int sum);
+
+int main(){
+char ch;
+int sum;
+isAlphabet(ch,sum);
+    return 0;
+}
 void isAlphabet(char ch, int sum){
 
 srand(time(NULL));
@@ -9,12 +17,4 @@ sum = 65 <= ch && ch <= 90 || 97 <= ch && ch <= 122;
 
 printf("random character: %c\n",ch);
 printf("%c is alphabet(1) or not(0): %d ",ch,sum);
-}
-
-int main(){
-char ch;
-int sum;
-isAlphabet(ch,sum);
-    return 0;
-}
-    
+} 
